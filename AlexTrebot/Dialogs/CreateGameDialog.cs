2f23e8c7e05e71f5e0bb34391a3a6ebb7bac5d28
Kyle Hildebrandt <kylehild@umich.edu>
2018-03-20 08:27:51 -0700

    Add CreateGameDialog Skeleton

---
+++ AlexTrebot/Dialogs/CreateGameDialog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Net.Http;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AlexTrebot.Dialogs
+{
+    [Serializable]
+    public class CreateGameDialog : IDialog<string>
+    {
+        private List<string> arguments;
+
+        public CreateGameDialog(string message)
+        {
+            this.arguments = message.Split(' ').ToList();
+        }
+
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var result = await argument;
+
+            if (arguments.Count() < 2)
+            {
+                context.Done($"Your input does not contain all expected arguments: {result.Text}");
+            }
+            else {
+                context.Done($"Let's make a game! {result.Text}");
+            }
+        }
+    }
+}
--- AlexTrebot/Dialogs/RootDialog.cs
+++ AlexTrebot/Dialogs/RootDialog.cs
@@ -13,9 +13,11 @@ namespace AlexTrebot.Dialogs
     {
         protected int count = 1;
 
-        public async Task StartAsync(IDialogContext context)
+        public Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
         }
 
         //This is the "entry" point for all messages recieved by this bot. 
@@ -25,11 +27,12 @@ namespace AlexTrebot.Dialogs
             var lowercasemessage = message.Text.ToLower();
 
             //Decide what dialog to call based on the contents of the first message
-            if (lowercasemessage.Contains("creategame"))
+            if (lowercasemessage.Contains("create"))
             {
-                await context.PostAsync($"Add the new game dialog here!");
-                // call a new dialog here e.g.:
-                // await context.Forward(new NewOrderDialog(), this.ResumeAfterNewOrderDialog, message, CancellationToken.None);
+                await context.Forward(new CreateGameDialog(lowercasemessage), 
+                                      this.ResumeAfterDialog, 
+                                      message, 
+                                      CancellationToken.None);
             }
             else if (lowercasemessage.Contains("answer"))
             {
@@ -49,22 +52,24 @@ namespace AlexTrebot.Dialogs
             //}
             else
             {
-                await context.PostAsync($"That is not a command I recognize! Possible commands are creategame, answer, viewResponses...etc..we're still working on it");
+                await context.PostAsync(
+                    $"That is not a command I recognize! Possible commands are " +
+                    "create, answer, viewResponses...etc..we're still working on it");
                 context.Wait(this.MessageReceivedAsync);
             }
         }
 
         //e.g of a "Resume After dialog" function.
-        //private async Task ResumeAfterEchoDialog(IDialogContext context, IAwaitable<string> result)
-        //{
-        //    // Store the value that NewOrderDialog returned. 
-        //    // (At this point, new order dialog has finished and returned some value to use within the root dialog.)
-        //    string resultFromEchoDialog = await result;
+        private async Task ResumeAfterDialog(IDialogContext context, IAwaitable<string> result)
+        {
+            // Store the value that NewOrderDialog returned. 
+            // (At this point, new order dialog has finished and returned some value to use within the root dialog.)
+            string resultFromEchoDialog = await result;
 
-        //    await context.PostAsync(resultFromEchoDialog);
+            await context.PostAsync(resultFromEchoDialog);
 
-        //    // Again, wait for the next message from the user.
-        //    context.Wait(this.MessageReceivedAsync);
-        //}
+            // Again, wait for the next message from the user.
+            context.Wait(this.MessageReceivedAsync);
+        }
     }
 }
